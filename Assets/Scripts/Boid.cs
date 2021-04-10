77e3ffd9e5aa17c1bc679b9ecbc05423e4c5e64a
Wenhe Qi <wenhe.qi@outlook.com>
2021-04-10 15:11:42 -0700

    Implement cohesion rule

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -4,28 +4,89 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-    private static float MAX_SPEED = 2f;
+    private static float MAX_SPEED = 1f;
+    private static float MAX_FORCE = 2f;
+    private static float PERCEPTION_DIST = 1;
     [SerializeField]
     private Vector3 velocity; // world space velocity
+    private float mass;
+    /*
+     * Move forward based on current velocity
+     */
+    public void Move()
+    {
+        transform.Translate(velocity * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.LookRotation(velocity);
+    }
+
+    /*
+     * Move based on Craig Reynolds' three rules
+     */
+    public void MoveInFlock(List<Transform> flock)
+    {
+        Vector3 steeringDir;
+        Vector3 steeringForce;
+        Vector3 acceleration;
+
+        steeringDir = this.Cohere(flock);
+        steeringDir.Normalize();
+
+        // steering_force = truncate (steering_direction, max_force)
+        steeringForce = Vector3.ClampMagnitude(steeringDir * MAX_FORCE, MAX_FORCE);
+        // acceleration = steering_force / mass
+        acceleration = steeringForce / mass;
+        // velocity = truncate(velocity + acceleration, max_speed)
+        velocity = Vector3.ClampMagnitude(velocity + acceleration * Time.deltaTime, MAX_SPEED);
+
+        Move();
+    }
+
+    /*
+     * Flock centering makes a boid want to be near the center of the flock.
+     * Because each boid has a localized perception of the world, "center of the
+     * flock" actually means the center of the nearby flockmates. Flock 
+     * centering causes the boid to fly in a direction that moves it closer to 
+     * the centroid of the nearby boids. If a boid is deep inside a flock, the 
+     * population density in its neighborhood is roughly homogeneous; the boid 
+     * density is approximately the same in all directions. In this case, the 
+     * centroid of the neighborhood boids is approximately at the center of the 
+     * neighborhood, so the flock centering urge is small. But if a boid is on 
+     * the boundary of the flock, its neighboring boids are on one side. The 
+     * centroid of the neighborhood boids is displaced from the center of the 
+     * neighborhood toward the body of the flock. Here the flock centering urge 
+     * is stronger and the flight path will be deflected somewhat toward the 
+     * local flock center.
+     */
+    private Vector3 Cohere(List<Transform> flockmates)
+    {
+        Vector3 steeringDir;
+        Vector3 centroid = Vector3.zero;
+
+        foreach(Transform flockmate in flockmates)
+        {
+            // exclude boid itself from blockmates
+            if (flockmate.position != transform.position &&
+                Vector3.Distance(this.transform.position,
+                flockmate.position) <= PERCEPTION_DIST)
+            {
+                // positon is in world space
+                centroid += flockmate.position;
+            }
+        }
+
+        centroid /= flockmates.Count;
+
+        steeringDir = (centroid - transform.position) - velocity;
+        steeringDir.Normalize();
+
+        return steeringDir;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        velocity = Vector3.ClampMagnitude(
-                new Vector3(
-                    Random.Range(-MAX_SPEED, MAX_SPEED),
-                    Random.Range(-MAX_SPEED, MAX_SPEED),
-                    Random.Range(-MAX_SPEED, MAX_SPEED)
-                ),
-                MAX_SPEED
-            );
-        transform.rotation = Quaternion.LookRotation(velocity);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(velocity * Time.deltaTime, Space.World);
+        mass = 1f;
+        velocity = Random.onUnitSphere * MAX_SPEED;
         transform.rotation = Quaternion.LookRotation(velocity);
     }
 }
--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -25,6 +25,18 @@ public class Flock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        List<Transform> transformList = new List<Transform>();
+
+        // make a copy of the transforms of each boid
+        foreach (Boid b in boids)
+        {
+            transformList.Add(b.transform);
+        }
+
+        foreach(Boid b in boids)
+        {
+            //b.Move();
+            b.MoveInFlock(transformList);
+        }
     }
 }
