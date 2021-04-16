afce4909111cc14c1a38d3e993decf4b3ae15466
unknown <csingley@usc.edu>
2021-04-16 13:23:39 -0400

    fixed wall

---
+++ Assets/Environment.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Environment : MonoBehaviour
+{
+    public GameObject wall;
+
+
+    public void createWall()
+    {
+        wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall.transform.position = new Vector3(0, 0, -50);
+        wall.transform.localScale = new Vector3(200, 200, 25);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createWall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
