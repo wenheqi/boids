486692b6e9a06a7a4e0dedd091134e28439053d2
Wenhe Qi <wenhe.qi@outlook.com>
2021-04-18 13:24:28 -0700

    Create test cases for bin lattice

--- Assets/Scripts/SpatialPartitionTester.cs
+++ Assets/Scripts/SpatialPartitionTester.cs
@@ -4,12 +4,77 @@ using UnityEngine;
 
 public class SpatialPartitionTester : MonoBehaviour
 {
-    private BinLattice<int> bl;
+    private BinLattice<float> bl;
 
     // Start is called before the first frame update
     void Start()
     {
-        bl = new BinLattice<int>(Vector3.zero, 50f, 10);
+        bl = new BinLattice<float>(Vector3.zero, 500f, 10);
+        
+        // initialize elements
+        for (float x = 11f; x < 200f; x+=7f)
+        {
+            for (float y = 3f; y < 300f; y += 9f)
+            {
+                for (float z = 25f; z < 900f; z += 13f)
+                {
+                    Vector3 pos = new Vector3(x/y, z/y, z/7f);
+                    int index = bl.GetBinIndex(pos);
+                    if (index >= 0)
+                    {
+                        bl.Add(index, pos.magnitude);
+                    }
+                }
+            }
+        }
+
+        // check if each element is in the bin
+        for (float x = 11f; x < 200f; x += 7f)
+        {
+            for (float y = 3f; y < 300f; y += 9f)
+            {
+                for (float z = 25f; z < 900f; z += 13f)
+                {
+                    Vector3 pos = new Vector3(x / y, z / y, z / 7f);
+                    int index = bl.GetBinIndex(pos);
+                    if (index >= 0)
+                    {
+                        if (!bl.Contains(index, pos.magnitude))
+                        {
+                            Debug.Log(pos.magnitude + " should be in bin " +
+                                "index " + index);
+                            throw new System.Exception("test failed");
+                        }
+                        // Debug.Log(pos.magnitude + " looks good");
+                    }
+                }
+            }
+        }
+
+        bl = new BinLattice<float>(Vector3.zero, 15f, 3);
+        int count = 0;
+        for (int y = 0; y < 15; y+=5) 
+        {
+            for (int z = 0; z < 15; z += 5)
+            {
+                for (int x = 0; x < 15; x += 5)
+                {
+                    bl.Add(new Vector3(x + Random.Range(0,1f),
+                                       y + Random.Range(0, 1f),
+                                       z + Random.Range(0, 1f)), count++);
+                }
+            }
+        }
+        
+        List<float> list = bl.QueryCube(new Vector3(7.5f, 7.5f, 0), 10f);
+        Debug.Log("list length " + list.Count); // expect 18
+        Debug.Log(System.String.Join(", ", list));
+        list = bl.QueryCube(new Vector3(7.5f, 7.5f, 0), 9f);
+        Debug.Log("list length " + list.Count); // expect 9
+        Debug.Log(System.String.Join(", ", list));
+        list = bl.QueryCube(new Vector3(0.5f, 0.7f, 4.99f), 9f);
+        Debug.Log("list length " + list.Count); // expect 4
+        Debug.Log(System.String.Join(", ", list));
     }
 
     // Update is called once per frame
