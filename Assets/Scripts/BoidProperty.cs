7b746ed92bc0745bf1780e7ac61ae88ec9cb802f
Wenhe Qi <wenhe.qi@outlook.com>
2021-04-18 13:23:28 -0700

    Override Equals and GetHashCode for HashSet use

--- Assets/Scripts/BoidProperty.cs
+++ Assets/Scripts/BoidProperty.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,5 +16,37 @@ public class BoidProperty
         rotation = boid.transform.rotation;
         velocity = boid.GetVelocity();
         forward = boid.transform.forward;
+    }
+
+    public override bool Equals(object obj)
+    {
+        // compare an instance with itself
+        if (System.Object.ReferenceEquals(obj, this))
+            return true;
+
+        BoidProperty other = obj as BoidProperty;
+
+        if (System.Object.ReferenceEquals(null, other))
+            return false;
+
+        if (position.Equals(other.position) &&
+            rotation.Equals(other.rotation) &&
+            velocity.Equals(other.velocity) &&
+            forward.Equals(other.forward))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        long hash = position.GetHashCode() +
+                    rotation.GetHashCode() +
+                    velocity.GetHashCode() +
+                    forward.GetHashCode();
+        return hash > Int32.MaxValue ?
+               Convert.ToInt32(hash % Int32.MaxValue) :
+               Convert.ToInt32(hash);
     }
 }
